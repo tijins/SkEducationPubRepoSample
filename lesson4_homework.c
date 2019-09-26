b910df6f91bc19b2c414b476051020b890e95a83
Toshihiro Sekioka <esper776@hotmail.com>
2019-09-26 22:59:16 +0900

    エラーが出ない状態まで修正しました。
    3.1 文字列の比較は、他の方法を試してください。
    ヒント　strcmp()

--- lesson4_homework.c
+++ lesson4_homework.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 #include <string.h>
 
-int main();
+//関岡 不要な;があります
+int main()
 {
     int i = 32;
     char *str = "ABCDEFG";
     char *str1 = "ABC";
     char *str2 = "ABC";
 
-    printf("i=%d\n", i):
+    //関岡 ;（セミコロン）と:（コロン）が誤字になっています
+    printf("i=%d\n", i);
     printf("i=%02x\n", i);
     printf("i=%p\n", i);
 
     printf("%s %d\n", str, strlen(str));
 
+    //関岡 この方法では、文字列のアドレス（ポインタ）同時を比較しています。
     if(str1 == str2){
         printf("true\n");
     }else{
